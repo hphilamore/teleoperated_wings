18910fe30c1046ec14490ded8ed2532fb19c3c44
hphilamore <hemmaphilamore@gmail.com>
2023-04-03 18:30:15 +0100

    DC motor stuff removed from server code

--- telepresence-server.py
+++ telepresence-server.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
 import socket
-from gpiozero import Motor, OutputDevice
+# from gpiozero import Motor, OutputDevice
 from time import sleep
 from time import time
 
 # Define and setup GPIO pins
 # Rigth foot
-motor1 = Motor(24, 27)
-motor1_enable = OutputDevice(5, initial_value=1)
-# Left foot
-motor2 = Motor(6, 22)
-motor2_enable = OutputDevice(17, initial_value=1)
-# Right tentacle
-motor3 = Motor(23, 16)
-motor3_enable = OutputDevice(12, initial_value=1)
-# Left tentacle
-motor4 = Motor(13, 18)
-motor4_enable = OutputDevice(25, initial_value=1) 
+# motor1 = Motor(24, 27)
+# motor1_enable = OutputDevice(5, initial_value=1)
+# # Left foot
+# motor2 = Motor(6, 22)
+# motor2_enable = OutputDevice(17, initial_value=1)
+# # Right tentacle
+# motor3 = Motor(23, 16)
+# motor3_enable = OutputDevice(12, initial_value=1)
+# # Left tentacle
+# motor4 = Motor(13, 18)
+# motor4_enable = OutputDevice(25, initial_value=1) 
 
 # HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 HOST = "0.0.0.0"  # Listen on all interfaces
@@ -68,22 +68,22 @@ while(1):
 
         while True:
 
-            time_new = time()
-            if time_new-time_old >= 2:
-                flag_wings = not flag_wings
-                time_old = time_new
-                print('switched wing direction')
-                #sleep(2)
+            # time_new = time()
+            # if time_new-time_old >= 2:
+            #     flag_wings = not flag_wings
+            #     time_old = time_new
+            #     print('switched wing direction')
+            #     #sleep(2)
 
-            # # Switch wings on/off
-                if flag_wings:
-                    print('wings up')
-                    motor3.forward()
-                    motor4.forward()
-                else:
-                    print('wings down')
-                    motor3.stop()
-                    motor4.stop()
+            # # # Switch wings on/off
+            #     if flag_wings:
+            #         print('wings up')
+            #         motor3.forward()
+            #         motor4.forward()
+            #     else:
+            #         print('wings down')
+            #         motor3.stop()
+            #         motor4.stop()
 
 
 
@@ -94,24 +94,24 @@ while(1):
             msg = data.decode()
             print(msg)
 
-            if msg == 'stop':
-                motor1.stop() 
-                motor2.stop()
-                motor3.stop() 
-                motor4.stop()
+            # if msg == 'stop':
+            #     motor1.stop() 
+            #     motor2.stop()
+            #     motor3.stop() 
+            #     motor4.stop()
 
-            elif msg == 'left':
-                motor1.stop()
-                motor2.forward(0.5)
+            # elif msg == 'left':
+            #     motor1.stop()
+            #     motor2.forward(0.5)
 
-            elif msg == 'right':
-                motor1.forward(0.5)
-                motor2.stop()
+            # elif msg == 'right':
+            #     motor1.forward(0.5)
+            #     motor2.stop()
                 
 
-            elif msg == 'forward':
-                motor1.forward(0.5)
-                motor2.forward(0.5)
+            # elif msg == 'forward':
+            #     motor1.forward(0.5)
+            #     motor2.forward(0.5)
 
             #conn.sendall(data)
     
