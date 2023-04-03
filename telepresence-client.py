db30b69ec08c1cfe4a1c2fdb4fd03228eb6aa3f8
hphilamore <hemmaphilamore@gmail.com>
2023-04-03 18:40:10 +0100

    servo stuff added to server code

--- telepresence-client.py
+++ telepresence-client.py
@@ -24,7 +24,7 @@ drawingModule = mediapipe.solutions.drawing_utils
 handsModule = mediapipe.solutions.hands
 
 HOST = "192.168.134.93"  # The raspberry pi's hostname or IP address
-PORT = 65442            # The port used by the server
+PORT = 65443            # The port used by the server
 
 flag_no_hand = False 
 
